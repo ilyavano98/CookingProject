492321c1def4c3b77ff08ffaf9481c2542032a2e
ilya1342 <ilya13427900@mail.ru>
2022-03-09 21:24:03 +0700

    Добавлены ссылки и контроллеры для обработки открытия ссылок, также сделан пример того, как работает один из способов передачи переменных с одной страницы на другю

--- src/main/java/com/example/CookingProject/controllers/MainController.java
+++ src/main/java/com/example/CookingProject/controllers/MainController.java
@@ -2,17 +2,53 @@ package com.example.CookingProject.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String greeting(Model model) {
+    public String firstPagaOpen(Model model) {
         String title = "Главная страница";
         String name = "Диман, ты тут?";
         model.addAttribute("title", title);
         model.addAttribute("name", name);
         return "index";
+    }
+
+    @GetMapping("/game")
+    public String gameOpen(@RequestParam String firstName, Model model) {
+        String title = "Игровая страница";
+        model.addAttribute("title", title);
+        model.addAttribute("name", firstName);
+        return "game";
+    }
+
+    @GetMapping("/reit")
+    public String openReit(Model model) {
+        String title = "Страница рейтинга";
+        String name = "тут таблица рейтинга";
+        model.addAttribute("title", title);
+        model.addAttribute("name", name);
+        return "game";
+    }
+
+    @GetMapping("/reg")
+    public String regPageOpen(Model model) {
+        String title = "Регистрация";
+        String name = "тут тело страницы выхода";
+        model.addAttribute("title", title);
+        model.addAttribute("name", name);
+        return "reg";
+    }
+
+    @GetMapping("/out")
+    public String outPage(Model model) {
+        String title = "Страница обработки выхода с аккаунта";
+        String name = "а эта тсраница будет скваозная, потому что ее код будет выполняться в джаве";
+        model.addAttribute("title", title);
+        model.addAttribute("name", name);
+        return "out";
     }
 
 }
